8cfeda49231ed30780d1c4a3bbcd7efbcd4aace9
KylePristine <bulzevalexandr@yandex.ru>
2023-11-27 16:58:07 +0300

    V1.1.1

--- js/like-script.js
+++ js/like-script.js
@@ -3,6 +3,9 @@ let posts = document.querySelectorAll(".post")
 posts.forEach((post) => {
     let likeBtn = post.querySelector(".like-icon")
     likeBtn.addEventListener("click", e => {
-        likeBtn.classList.toggle('liked')
+        likeBtn.classList.toggle("liked")
+        if (likeBtn.classList.contains("liked")) {
+            Notie("Новая отметка нравится")
+        }
     })
 })
--- js/show-noties.js
+++ js/show-noties.js
@@ -13,30 +13,11 @@ function Notie(content, href) {
             notiesButton.classList.toggle("pressed")
         }
     })
-    return notie
-}
-
-function showNotie(notie) {
     container.append(notie)
 }
 
-function countedFunction(func, maxCount) {
-    let count = 0;
-    return function() {
-        if (count < maxCount) {
-            count += 1
-            return func.call(this, ...arguments)
-        }
-    }
-}
-showNotie = countedFunction(showNotie, 10)
-
 notiesButton.addEventListener("click", e => {
-    notiesButton.classList.toggle("pressed")
-    
-    if (notiesButton.classList.contains("pressed")) {
-        timer = setInterval(() => {showNotie(Notie("Ошибка"))}, 1000)
-    } else {
-        clearInterval(timer)
+    if (container.children.length != 0) {
+        notiesButton.classList.toggle("pressed")
     }
 })
