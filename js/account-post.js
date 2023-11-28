1958d1a1b3dd79d6773509b5cf902b8b9a19614d
Desktop <bulzevalexandr@yandex.ru>
2023-11-28 13:20:17 +0300

    V1.1.3

--- js/account-post.js
+++ js/account-post.js
@@ -1,5 +1,6 @@
 let newPostPhoto = document.querySelector('.new-post img')
 let newPostPhotoInput = document.querySelector('.new-post input[type=file]')
+
 newPostPhotoInput.addEventListener("change", e => {
 	let photo = newPostPhotoInput.files[0]
     if (photo) {
